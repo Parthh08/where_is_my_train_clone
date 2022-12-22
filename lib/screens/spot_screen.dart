409b987cdaf1f0f5d354c92942e20121ee6d8ca7
Parth <www.parthsingh0808@gmail.com>
2022-12-22 22:24:08 +0530

    Added Station Selector

--- lib/screens/spot_screen.dart
+++ lib/screens/spot_screen.dart
@@ -11,31 +11,58 @@ class SpotScreen extends StatefulWidget {
 class _SpotScreenState extends State<SpotScreen> {
   get border => null;
 
+  get child => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.only(left: 14, right: 14, top: 20, bottom: 40),
+        padding:
+            const EdgeInsets.only(left: 14, right: 14, top: 20, bottom: 40),
         child: Stack(
           children: [
             Container(
               height: 180,
               width: 500,
               decoration: const BoxDecoration(
-                color:  Colors.white,
-
+                color: Colors.white,
               ),
             ),
-           const Text("Parth Singh"),
-           const Padding(
-             padding: EdgeInsets.only(top: 86, left: 60),
-             child: Divider(height: 1
-               , thickness: 1.4,),
-           ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(20.0),
+                ),
+                Row(
+                  children: const [Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Icon(Icons.circle_outlined),
+                  ), Text("Raipur")],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40, left: 56),
+                  child: Stack(children: const [
+                    Divider(
+                      height: 1,
+                      thickness: 1.4,
+                    ),
+                    Icon(Icons.mobiledata_off)
+                  ]),
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Icon(Icons.circle_outlined),
+                    ), Text("Bihar")
+                  ],
+                )
+              ],
+            ),
           ],
-        )
+        ),
       ),
     );
   }
 }
-
